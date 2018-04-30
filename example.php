59f2b81e7199ba57bb847be30bda8fe1d27da6b1
Giuseppe <peppelg1234@gmail.com>
2018-04-30 21:50:00 +0200

    .

--- example.php
+++ example.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'vendor/autoload.php';
+//require_once 'vendor/autoload.php';          da usare per composer
+require_once 'MultiProto.phar';              //prima scarica Multiproto.phar
 $MultiProto = new MultiProto(glob('session/*.madeline'));
 $MultiProto->exec(function($MadelineProto, $me) {
   $MadelineProto->messages->sendMessage(['peer' => '@peppelg1', 'message' => 'ciao!1!']); //tutti gli userbot salutano @peppelg1
